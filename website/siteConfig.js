822eb5497b5ee2a9067b83469346fe90458f696c
rganel <31194850+rganel@users.noreply.github.com>
2020-10-02 12:24:59 -0700

    Update siteConfig.js
    Update url for website

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -15,7 +15,7 @@ const siteConfig = {
     title: 'Your Name Here', // Title for your website.
     tagline: 'Continuously improving, aspiring technologist.',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://rganel.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
